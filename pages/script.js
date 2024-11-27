3fc87f533d8d442d27d8fa06ef953de00eb5a2c2
lokChingt <C24385243@myTUDublin.ie>
2024-11-27 20:56:46 +0000

    Update contact page, CSS and Javascript
    - Added first name, last name, reasons (dropdown) to the contact form
    - Changed the type of submit button from "button" to "submit"
    - Changed the social media to links with colour
    - Added light-mode to footer and socialMedia links

--- pages/script.js
+++ pages/script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript file loaded and ready");
-  
-    document.querySelector('.menu-toggle').addEventListener('click', function() {
+  console.log("JavaScript file loaded and ready");
+
+  document.querySelector('.menu-toggle').addEventListener('click', function() {
       console.log('Button clicked!');
       document.querySelector('.navbar').classList.toggle('active');
-    });
+  });
 });
 
 function toggleLightMode() {
-    document.body.classList.toggle('light-mode');
-}  
+  document.body.classList.toggle('light-mode');
+  document.querySelector('footer').classList.toggle('footer_light-mode');
+  document.querySelector('.socialMedia').classList.toggle('socialMedia_light-mode');
+}
