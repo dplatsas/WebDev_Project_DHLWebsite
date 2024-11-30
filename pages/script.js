65fb46c1a98f631b7bfb94efea34d46a4250c0c5
lokChingt <C24385243@myTUDublin.ie>
2024-11-30 20:45:46 +0000

    Update all pages
    - Changed the form placeholder
    - Added submissionMessage when form is submitted, it will show for 3 seconds
    - Fixed some responsive issues

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("JavaScript file loaded and ready");
+//hamburger-menu
+function toggleMenu() {
+  document.querySelector('.navbar').classList.toggle('active');
+  console.log('Menu clicked!');
+}
 
-  document.querySelector('.menu-toggle').addEventListener('click', function() {
-      console.log('Button clicked!');
-      document.querySelector('.navbar').classList.toggle('active');
-  });
-});
 
+//light-mode
 function toggleLightMode() {
   document.body.classList.toggle('light-mode');
   document.querySelector('footer').classList.toggle('footer_light-mode');
   document.querySelector('.socialMedia').classList.toggle('socialMedia_light-mode');
 }
+
+//submission-message
+let form = document.querySelector('.enquiry');
+
+form.onsubmit = function (event) {
+  event.preventDefault(); // prevent the page to be refreshed
+  console.log('Submit Clicked!');
+
+  const submitButton = document.querySelector('.submit');
+  submitButton.classList.add('showMessage');
+
+  //show for 3 seconds, then reset the form
+  let countdown = 3;
+  const interval = setInterval(() => {
+    console.log(`Message will disappear in ${countdown} seconds`);
+    countdown--;
+
+    if (countdown < 0) {
+      clearInterval(interval);
+      submitButton.classList.remove('showMessage');
+      form.reset();
+      console.log('Message hidden and form reset.');
+    }
+  }, 1000);
+};
